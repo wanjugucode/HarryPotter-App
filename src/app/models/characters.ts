2bd0e6748afd19944ef846baf5ffdff4fbcd45aa
wanjugucode <karobiaanastasia@gmail.com>
2023-08-17 22:34:12 +0300

    files

---
+++ src/app/models/characters.ts
@@ -0,0 +1,29 @@
+import { Url } from "url";
+
+export interface Characters{
+    id:string;
+    name:string;
+
+    dateOfBirth:string;
+    house:string;
+    wand:{
+        wood:string;
+        core:string;
+        length:number;
+    }
+    alternate_names:[];
+    yearOfBirth:number;
+    species:string;
+    wizard:boolean;
+    ancestry:string;
+    eyeColour:string;
+    hairColour:string;
+    patronus:string
+    hogwartsStudent:boolean;
+    actor:string;
+    alternate_actors:[];
+    alive:boolean;
+    image:string
+
+
+}
