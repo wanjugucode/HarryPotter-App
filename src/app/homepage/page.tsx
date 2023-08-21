57767f5943ad465db5ec2b492c3e3475779eda2b
wanjugucode <karobiaanastasia@gmail.com>
2023-08-21 07:07:32 +0300

    page.tsx

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { Characters } from "./models/characters";
+import { Characters } from "../models/characters";
 import Link from "next/link";
 import "tailwindcss/tailwind.css";
 import Styles from "../styles/styles.module.css";
